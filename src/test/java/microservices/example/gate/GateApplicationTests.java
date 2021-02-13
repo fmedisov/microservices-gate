ff7146a678de26b3cdac1b0c0578855e79b2b00e
fmedisov <fmedisov@gmail.com>
2021-02-13 23:15:30 +0300

    initial commit

---
+++ src/test/java/microservices/example/gate/GateApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.example.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
