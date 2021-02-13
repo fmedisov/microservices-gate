e9530ca71f1cdf68d368ca68596bb036effcab2b
fmedisov <fmedisov@gmail.com>
2021-02-13 23:18:11 +0300

    implementation

---
+++ src/main/java/microservices/example/gate/EndpointController.java
@@ -0,0 +1,24 @@
+package microservices.example.gate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EndpointController {
+
+    private final BackendAdapter backendAdapter;
+
+    @Value("${instance.id}")
+    private int instanceId;
+
+    @Value("${secret}")
+    private String secret;
+
+    @GetMapping("/")
+    public String getRequestsCount() {
+        return String.format("Number of requests %s (gateway %d, secret %s)", backendAdapter.getRequests(), instanceId, secret);
+    }
+}
